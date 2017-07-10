b1b0e5abcfe2f369651208c05abe25b86300b0f1
jaotors <turingan.joshua@gmail.com>
2017-07-10 17:04:40 +0800

    add validations of some inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 (function() {
   
   let inputZip = document.getElementById('zipcode')
-  let container = document.getElementById('continue-container')
+  let container = document.getElementById('container')
+  let newContainer = document.getElementById('continue-container')
   let containerCont = false
+  let errCodes = [
+    `Unfortunately there is no ZIP code like that in makati`,
+    `ZIP code must be 4 digits`,
+    `Hey youngster, you have to be 18 or older to sign up with Oscar`,
+    `Your spouse needs to be at least 18 years old`,
+    `Your age is required to get a quote`,
+    `Your spouse’s age is required to get a quote`,
+    `All ages are required to get a quote`
+  ]
+  let options = ['', 'me', 'me and my spouse', 'me, my spouse and kids', 'me and my kids']
+  const zipcodes = ["1226","1233","1209","1214","1217","1221","1222","1219","1220","1202","1201","1228","1212","1211","1206","1204","1229","1232","1224","1200","1207","1235","1231","1218","1213","1230","1210","1216","1215","1227","1203","1234","1223","1205","1225","1208"]
 
-  let needInput = (num) => {
+  let childrenInput = (num) => {
     let par = document.createElement('span')
     par.id = 'kidsAgeContainer'
     if(num == 1) {
@@ -20,37 +32,101 @@
         } else if (i == num) {
           par.innerHTML += `<input class="kidsAge" type="text">.`
         }
-
       }
     }
 
     return par
   }
 
-  let validateInput = (e) => {
+  let validateNumbersOnly = (e) => {
     if (!e.key.match(/([0-9])/g)) e.preventDefault()
-    if (e.target.value < 5)
   }
 
-  let validationAge18 = (e) => {
+  let validateZipCodeLength = (e) => {
+    if(e.target.value.length > 3) e.preventDefault()
+  }
 
+  let validateZipCode = (e) => {
+    let error = []
+    const {value} = e.target
+
+    if(!zipcodes.includes(value)) {
+      error.push(errCodes[0])
+    }
+
+    if(value.length <= 3) {
+      error.push(errCodes[1]) 
+    }
+
+    return error
+  }
+
+  let validateAge = (e) => {
+    const {name, value} = e.target
+    let error = []
+
+    if(value < 18) {
+      if(name === 'your') {
+        error.push(errCodes[2])
+      } else if(name === 'spouse') {
+        error.push(errCodes[3])
+      }
+    }
+    return error
+  }
+
+  let putError = (errors, sibling) => {
+    let errCont = document.getElementsByClassName(`error-${sibling}`)
+    if(errCont.length > 0) {
+      for(err of errCont) {
+        err.remove()
+      }
+    }
+
+    let errUl = document.createElement('ul')
+    errUl.className += `error-${sibling} error`
+
+    if(errors.length > 0) {
+      errors.map((err) => {
+        let li = document.createElement('li')
+        li.innerHTML = err
+        errUl.append(li)
+      })
+
+      return errUl
+    }
+
+    return false
   }
 
   let validateRequired = (e) => {
-    
+    let err = ''
+    if(e.target.value == '') {
+      err = `All age is requried.`
+    }
+
+    return err
   }
   
   //start
-  inputZip.addEventListener('keypress', validateInput)
+  inputZip.addEventListener('keypress', (e) => {
+    validateNumbersOnly(e)
+    validateZipCodeLength(e)
+  })
+
   inputZip.addEventListener('keyup', (e) => {
-    
-    if(e.target.value === '12345' && containerCont === false)  {
+    let errors = validateZipCode(e)
+    let setErrors = putError(errors, 'zip')
+    if(setErrors) {
+      inputZip.after(setErrors)
+    }
+
+    if(zipcodes.includes(e.target.value) && containerCont === false) {
       containerCont = true
       let selectBox = document.createElement('select')
       let paragraph = document.createElement('p')
       paragraph.innerHTML = `<span>I'd like to cover</span>`
 
-      let options = ['', 'me', 'me and my spouse', 'me, my spouse and kids', 'me and my kids']
       let selectOptions = options.map((option, index) => {
         let opt = document.createElement('option')
 
@@ -68,34 +144,73 @@
         selectBox.append(option)
       }).reverse()
 
-      selectBox.append(selectOptions)
       paragraph.append(selectBox)
-      container.append(paragraph)
+      newContainer.append(paragraph)
 
       selectBox.addEventListener('change', (e) => {
+        let errors = []
         const { value } = e.target
         let ageSelect = document.getElementById('ageSelect')
-        if(ageSelect != null) container.removeChild(ageSelect)
+        if(ageSelect != null) newContainer.removeChild(ageSelect)
         let paragraph = document.createElement('p')
         paragraph.id = 'ageSelect'
 
 
         if(value == 1) {
-          paragraph.innerHTML = `I'm <input id="meAge" type="text"> years old`
+          paragraph.innerHTML = `I'm <input name="your" id="meAge" type="text"> years old`
         } else if (value == 2) {
-          paragraph.innerHTML = `I'm <input id="meAge" type="text"> years old and my spouse is <input id="spouseAge" type="text">`
+          paragraph.innerHTML = `I'm <input name="your" id="meAge" type="text"> years old and my spouse is <input name="spouse" id="spouseAge" type="text">`
         } else if (value == 3) {
-          paragraph.innerHTML = `I'm <input id="meAge" type="text"> 
-                                  years old and my spouse is <input id="spouseAge" type="text"> 
+          paragraph.innerHTML = `I'm <input name="your" id="meAge" type="text"> 
+                                  years old and my spouse is <input name="spouse" id="spouseAge" type="text"> 
                                   and my <select id="kidSelect"></select> <span id="replaceText">kid is</span>`
         } else if (value == 4) {
-          paragraph.innerHTML = `I'm <input id="meAge" type="text"> 
+          paragraph.innerHTML = `I'm <input name="your" id="meAge" type="text"> 
                                   years old and my <select id="kidSelect"></select> <span id="replaceText">kid is</span>`
         }
-        container.append(paragraph)
 
+        newContainer.append(paragraph)
+
+        let myAge = document.getElementById('meAge')
+        let spouseAge = document.getElementById('spouseAge')
         let kidSelect = document.getElementById('kidSelect')
+
+        if(myAge.value === '') {
+          errors.push(errCodes[4])
+
+          myAge.addEventListener('keypress', (event) => {
+            validateNumbersOnly(event)
+            validateZipCodeLength(event)
+          })
+
+          myAge.addEventListener('change', (event) => {
+            let errors = validateAge(event)
+            let setError =  putError(errors, 'age')
+            if(setError) {
+              ageSelect.after(setError)
+            } else {
+
+              
+
+            }
+          })
+        }
+
+        if(spouseAge != null) {
+          if(spouseAge.value === '') {
+            errors.push(errCodes[5])
+          }
+
+          spouseAge.addEventListener('keypress', (event) => {
+            validateNumbersOnly(event)
+            validateZipCodeLength(event)
+          })
+        }
+
         if(kidSelect != null) {
+
+          errors.push(errCodes[6])
+
           for(let i = 0; i < 10; i++) {
             let opt = document.createElement('option')
             opt.value = i
@@ -123,16 +238,26 @@
             } else {
               replaceText.innerHTML = 'kid are'
             }
-            replaceText.after(needInput(value))
+            replaceText.after(childrenInput(value))
 
             let kidsInput = document.getElementsByClassName('kidsAge')
 
             for(kidInput of kidsInput) {
-              kidInput.addEventListener('keyup', validateInput)
+              kidInput.addEventListener('keypress', (e) => {
+                validateNumbersOnly(e)
+                validateZipCodeLength(e)
+              })
             }
           })
         }
 
+        ageSelect = document.getElementById('ageSelect')
+        let setError =  putError(errors, 'age')
+        if(setError) {
+          ageSelect.after(setError)
+        } else {
+          ageSelect.after('')
+        }
       })
     }
   })
